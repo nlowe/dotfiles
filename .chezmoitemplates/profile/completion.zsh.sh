1a93ce0403d0e50b7b26e2cf1723ba81e8ba7bfb
Nathan Lowe <nathan@nlowe.dev>
2025-06-29 18:22:34 -0400

    hctl and fix go bin

--- .chezmoitemplates/profile/completion.zsh.sh
+++ .chezmoitemplates/profile/completion.zsh.sh
@@ -3,7 +3,10 @@ FPATH="${FPATH}:/usr/share/zsh/vendor-completions"
 autoload -Uz compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
+source <(hctl completion zsh)
 source <(kubectl completion zsh)
+
 compdef k='kubectl'
+compdef h='hctl'
 
 complete -o nospace -C /usr/local/bin/tk tk
